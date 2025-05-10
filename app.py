fffd4db869192f8b88ff8987d21595c1e5282f11
Jordan Capps <jordan@jordancapps.net>
2025-05-10 13:35:43 -0700

    undebug

--- app.py
+++ app.py
@@ -164,4 +164,4 @@ def index():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Use PORT from environment or default to 5000
-    app.run(host="0.0.0.0", port=port, debug=True)  # Enable debug mode
+    app.run(host="0.0.0.0", port=port, debug=False)  # Enable debug mode
